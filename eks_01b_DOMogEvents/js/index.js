9bd84e3461a46f8a763b73e26ddef84dd3849f13
orindholt <83012141+orindholt@users.noreply.github.com>
2021-08-17 14:19:55 +0200

    Add variable and remove hard code

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -1,6 +1,32 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	const plusBtn = document.querySelector("#btn-count-increase");
+	const minusBtn = document.querySelector("#btn-count-decrease");
+	const txtElement = document.querySelector("#text-count");
+	let count = 5;
+	let maxCount = 15;
+	let minCount = 0;
+
+	function updateCount(){
+		txtElement.textContent = count;
+	}
+	updateCount();
+
+	plusBtn.addEventListener("click", counter);
+	minusBtn.addEventListener("click", counter);
+
+	function counter(){
+		if(this.getAttribute("id")==="btn-count-increase"){
+			if(count < maxCount){
+				count++;
+			}
+		} else {
+			if(count > minCount){
+				count--;
+			}
+		}
+		updateCount();
+	}
 
 }); // Afslutter: DOMContentLoaded
