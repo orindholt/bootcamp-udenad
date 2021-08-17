98868bb97e36b076e91b6d7d865755f7b02c77b7
orindholt <83012141+orindholt@users.noreply.github.com>
2021-08-17 12:58:55 +0200

    Add assignment change 01a

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -1,7 +1,29 @@
 
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
+	const plusBtn = document.querySelector("#btn-count-increase");
+	const minusBtn = document.querySelector("#btn-count-decrease");
+	const txtElement = document.querySelector("#text-count");
+	let count = 5;
 
-	// skriv din kode her...
+	function updateCount(){
+		txtElement.textContent = count;
+	}
+	updateCount();
 
+	plusBtn.addEventListener("click", counter);
+	minusBtn.addEventListener("click", counter);
+
+	function counter(){
+		if(this.getAttribute("id")==="btn-count-increase"){
+			if(count < 10){
+				count++;
+			}
+		} else {
+			if(count > 0){
+				count--;
+			}
+		}
+		updateCount();
+	}
 }); // Afslutter: DOMContentLoaded
