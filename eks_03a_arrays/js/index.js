6d5c5199af5928ab12808e5f4bc9bfe36bc29b9a
orindholt <83012141+orindholt@users.noreply.github.com>
2021-08-19 14:42:42 +0200

    Add solution to assignment 03a

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -1,6 +1,15 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
+	const liste = document.querySelector("#liste");
+	arr = ["Lise", "Markus", "Julian"];
 
-	// skriv din kode her...
+	for(let i = 0; i<arr.length; i++){
+		makeElement(i);
+	}
 
+	function makeElement(i){
+		let element = document.createElement("li");
+		liste.append(element);
+		element.append(arr[i]);
+	}
 }); // Afslutter: DOMContentLoaded
