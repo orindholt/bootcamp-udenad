0ebb7ca17d0fe21b4b00ff4b854e01865c4fa7de
orindholt <83012141+orindholt@users.noreply.github.com>
2021-08-19 07:47:17 +0200

    Add solution to assignment 01c

--- eks_01c_DOMogEvents/js/index.js
+++ eks_01c_DOMogEvents/js/index.js
@@ -1,6 +1,50 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	const plusBtn = document.querySelector("#btn-count-increase");
+	const minusBtn = document.querySelector("#btn-count-decrease");
+	const txtElement = document.querySelector("#text-count");
+	let count = 5;
+	let maxCount = 15;
+	let minCount = 0;
+
+	function updateCount(){
+		txtElement.textContent = count;
+	}
+	updateCount();
+
+	function countCondition(event){
+		if(event.target.getAttribute("id")==="btn-count-increase"){
+			if(count < maxCount){
+				count++;
+			}
+		} else {
+			if(count > minCount){
+				count--;
+			}
+		}
+	}
+
+	plusBtn.addEventListener("click", (event) => {
+		countCondition(event);
+		updateCount();
+	});
+	minusBtn.addEventListener("click", (event) => {
+		countCondition(event);
+		updateCount();
+	});
+
+	/* function counter(){
+		if(this.getAttribute("id")==="btn-count-increase"){
+			if(count < maxCount){
+				count++;
+			}
+		} else {
+			if(count > minCount){
+				count--;
+			}
+		}
+		updateCount();
+	} */
 
 }); // Afslutter: DOMContentLoaded
