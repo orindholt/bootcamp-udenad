364589c6c32d20a3576cac349758daa12259c793
orindholt <83012141+orindholt@users.noreply.github.com>
2021-08-19 12:38:23 +0200

    Add solution to assignment 02c

--- eks_02c_funktioner/js/index.js
+++ eks_02c_funktioner/js/index.js
@@ -1,6 +1,10 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	function hils(navn){
+		return `Hej med dig, ${navn}`;
+	}
+	var hilsenFraMarkus = hils("Markus");
+	console.log(hilsenFraMarkus);
 
 }); // Afslutter: DOMContentLoaded
