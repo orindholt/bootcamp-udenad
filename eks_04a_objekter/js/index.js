7ae9dae25984122298105fbdf67c7d546bc05cef
orindholt <83012141+orindholt@users.noreply.github.com>
2021-08-20 12:30:41 +0200

    Remove unnecessary folder

--- eks_04a_objekter/js/index.js
+++
@@ -1,6 +0,0 @@
-// "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
-document.addEventListener("DOMContentLoaded", () => {
-
-	// skriv din kode her...
-
-}); // Afslutter: DOMContentLoaded
